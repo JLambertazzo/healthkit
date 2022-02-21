a933afc2453951b22a94d514b6c936624e23adc8
judy-n <judynaamani@icloud.com>
2022-02-21 01:04:59 -0500

    initial setup for dashboard

---
+++ client/src/views/Dashboard/Dashboard.js
@@ -0,0 +1,18 @@
+import './Dashboard.css'
+
+function Dashboard() {
+    return (
+        <div className="dashboard">
+
+
+            <div className="dash-side">
+
+            </div>
+            <div className="dash-main">
+                <h1>Dashboard</h1>
+            </div>
+        </div>
+    );
+}
+
+export default Dashboard;

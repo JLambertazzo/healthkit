387b75073ff3a7b82973b68a2c8597b8c4c68497
Julien <jbertazzolambert@gmail.com>
2022-02-21 22:14:43 -0500

    trying heroku setup

--- src/index.js
+++ src/index.js
@@ -21,7 +21,7 @@ app.get('*', (req, res, next) => {
     res.sendFile(path.join(__dirname, "../client/build/index.html"));
 })
 
-const port = 5000;
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
     console.log(`listening on ${port}`)
 })

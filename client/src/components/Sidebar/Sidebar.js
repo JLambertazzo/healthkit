ef63d23bb031dc34804027e897989e7e326a611d
judy-n <judynaamani@icloud.com>
2022-02-23 17:06:41 -0500

    added sidebar links + new dashboard page

---
+++ client/src/components/Sidebar/Sidebar.js
@@ -0,0 +1,16 @@
+import './Sidebar.css'
+import { Link } from 'react-router-dom';
+
+function Sidebar(props) {
+    return (
+        <div className="sidebar">
+            <ul className="side-nav">
+                <li className={props.current === 'recent' ? "current" : ""}><Link to='/'>Recent Forms</Link></li>
+                <li className={props.current === 'myforms' ? "current" : ""}><Link to='/myforms'>My Forms</Link></li>
+            </ul>
+
+        </div>
+    );
+}
+
+export default Sidebar;

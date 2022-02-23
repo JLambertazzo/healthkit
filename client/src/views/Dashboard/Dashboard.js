048261fa66e0ea3088c7dc72dba119ef81579ad1
judy-n <judynaamani@icloud.com>
2022-02-22 21:41:13 -0500

    navbar + some dashboard

--- client/src/views/Dashboard/Dashboard.js
+++ client/src/views/Dashboard/Dashboard.js
@@ -1,16 +1,25 @@
 import './Dashboard.css'
+import Navbar from "../../components/Navbar/Navbar";
+import Thumbnail from "../../components/Thumbnail/Thumbnail";
 
 function Dashboard() {
     return (
+        <div>
+        <Navbar/>
         <div className="dashboard">
 
 
             <div className="dash-side">
-
             </div>
             <div className="dash-main">
                 <h1>Dashboard</h1>
+                <div className="thumb-list">
+                    <Thumbnail/>
+                    <Thumbnail/>
+                    <Thumbnail/>
+                </div>
             </div>
+        </div>
         </div>
     );
 }

a2185171b178cf03d318aaea4209b1099f28f3af
judy-n <judynaamani@icloud.com>
2022-03-04 11:32:40 -0500

    make a new navbar/name

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -5,13 +5,16 @@ import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 function Navbar() {
     return (
         <div className="nav">
+
             <div className="navTitle">
-                <h2>Health Scores</h2>
+                <h2>Health Kit</h2>
+            </div>
+            <div>
             </div>
             <div className="navIcon">
                 <IconButton >
                     <AccountCircleIcon
-                        sx={{color: "#8C99A0", fontSize: '1.5em'}}
+                        sx={{color: "white", fontSize: '1.5em'}}
                     /></IconButton>
             </div>
 

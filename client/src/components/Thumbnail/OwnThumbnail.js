eb27a5decce86bcad00f672a1062d790c1132ed0
Julien <jbertazzolambert@gmail.com>
2022-03-29 13:20:37 -0400

    feat: delete forms in dashboard

--- client/src/components/Thumbnail/OwnThumbnail.js
+++ client/src/components/Thumbnail/OwnThumbnail.js
@@ -3,8 +3,10 @@ import Box from '@mui/material/Box';
 import IconButton from '@mui/material/IconButton';
 import VisibilityIcon from '@mui/icons-material/Visibility';
 import EditIcon from '@mui/icons-material/Edit';
+import DeleteIcon from '@mui/icons-material/Delete';
 import Tooltip from '@mui/material/Tooltip';
 import { useHistory } from 'react-router-dom';
+import { deleteForm } from '../../actions/form';
 
 function OwnThumbnail(props) {
     const history = useHistory()
@@ -22,6 +24,11 @@ function OwnThumbnail(props) {
                     <Tooltip title={"Edit"}>
                     <IconButton sx={{padding: '5px'}} onClick={() => history.push(`/editform/${props.id}`)}>
                         <EditIcon sx={{color: '#a9a9a9'}}/>
+                    </IconButton>
+                    </Tooltip>
+                    <Tooltip title={"Delete"}>
+                    <IconButton sx={{padding: '5px'}} onClick={props.onDelete}>
+                        <DeleteIcon sx={{color: '#a9a9a9'}}/>
                     </IconButton>
                     </Tooltip>
            </Box>

cc74431fc62578f972ccf3a6de883a730426d67d
judy-n <judynaamani@icloud.com>
2022-04-07 22:35:51 -0400

    fix: link on sidebar

--- client/src/components/Sidebar/Sidebar.js
+++ client/src/components/Sidebar/Sidebar.js
@@ -5,7 +5,7 @@ function Sidebar(props) {
     return (
         <div className="sidebar">
             <ul className="side-nav">
-                <li className={props.current === 'recent' ? "current" : ""}><Link to='/'>Recent Forms</Link></li>
+                <li className={props.current === 'recent' ? "current" : ""}><Link to='/home'>Recent Forms</Link></li>
                 <li className={props.current === 'myforms' ? "current" : ""}><Link to='/myforms'>My Forms</Link></li>
             </ul>
 

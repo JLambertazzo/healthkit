18b5973bb18f86e443dde764a5133cbec9c52a44
judy-n <judynaamani@icloud.com>
2022-04-28 01:24:04 -0400

    feat: add filler text in dashboard

--- client/src/views/Dashboard/Dashboard.js
+++ client/src/views/Dashboard/Dashboard.js
@@ -6,7 +6,8 @@ import Sidebar from "../../components/Sidebar/Sidebar";
 // import AddIcon from '@mui/icons-material/Add';
 import { useHistory } from 'react-router-dom'
 import {FaPlus} from "react-icons/fa";
-import {Flex, FormControl, FormLabel, Heading, HStack, Input, Box, Button} from "@chakra-ui/react";
+import {Flex, FormControl, FormLabel, Heading, HStack, Input, Box, Button, Text, Image} from "@chakra-ui/react";
+import empty from './emptydash.svg';
 
 function Dashboard(props) {
     const history = useHistory();
@@ -23,7 +24,11 @@ function Dashboard(props) {
             </div>
             <div className="dash-main">
                 <div className="thumb-list">
-                    {props.user.receivedForms.map((form) => {
+
+                    {props.user.receivedForms.length > 0 &&(
+
+                        props.user.receivedForms.map((form) => {
+                            console.log("got here")
                         return (<Thumbnail
                             form={form}
                             value={0}
@@ -31,7 +36,25 @@ function Dashboard(props) {
                             complete={form.isSubmitted}
                             title={form.name}
                         />)
-                    })}
+                    })
+                    )}
+                    {props.user.receivedForms.length === 0 && (
+                        <HStack
+                        w={'70%'}
+                        my={'2rem'}
+                        >
+                            <Image
+                            src={empty}
+                            boxSize={'150px'}
+                            />
+
+                            <Text
+                            color={'#2F8886'}
+                            marginLeft={'2rem !important'}
+                            fontWeight={500}
+                            >Nothing here yet! When someone shares a form with you to fill out, you’ll see it here.</Text>
+                        </HStack>
+                    )}
 
                 </div>
                 <div className="action-btn-cont">

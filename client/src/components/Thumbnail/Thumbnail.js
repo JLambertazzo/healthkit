5203d74f44853f97be737409a633584dbe5c84ee
Julien <jbertazzolambert@gmail.com>
2022-05-05 01:06:48 -0400

    feat: edit button on filled forms

--- client/src/components/Thumbnail/Thumbnail.js
+++ client/src/components/Thumbnail/Thumbnail.js
@@ -2,7 +2,7 @@ import './Thumbnail.css'
 import { VStack, Box, Button, Progress, HStack, Tooltip, IconButton } from "@chakra-ui/react";
 // import LinearProgress from '@mui/material/LinearProgress';
 import { Link } from 'react-router-dom';
-import {FaRegChartBar, FaShareAlt} from "react-icons/fa"
+import {FaRegChartBar, FaRegEdit, FaShareAlt} from "react-icons/fa"
 
 function Thumbnail(props) {
     return (
@@ -21,19 +21,22 @@ function Thumbnail(props) {
                           borderRadius={'sm'}
 
                 />
-                <p style={{fontSize: '12px', margin: '0 10px', color: '#8C99A0', width: 'fit-content'}}>{Math.round(props.value)}%</p>
-                    <p style={{fontSize: '12px', color: '#8C99A0'}}>Changed {props.date}</p>
+                <p style={{fontSize: '12px', margin: '0 10px', color: '#8C99A0', width: 'fit-content'}}>
+                    {Math.round(props.value)}%
+                </p>
+                <p style={{fontSize: '12px', color: '#8C99A0'}}>
+                    Changed {props.date}
+                </p>
             </Box>
             )}
             </Link>
             </Box>
             {props.complete && (<Box>
-                <Button rightIcon={<FaRegChartBar />}
-                        bg={'#cce3e3'} color={'#2a5555'}
-                _hover={{bg: '#bad5d5'}}
-                >
-                    Results
-                </Button>
+                <Link to={{pathname: "/form", state:{form:props.form}}} style={{ textDecoration: 'none' }}>
+                    <Button rightIcon={<FaRegEdit />} bg={'#cce3e3'} color={'#2a5555'} _hover={{bg: '#bad5d5'}}>
+                        Edit
+                    </Button>
+                </Link>
             </Box>)}
             <HStack>
                 <Tooltip label={"Share"}>

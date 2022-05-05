2dcf48b0bb69dfc81c630f4dcedf99c69397c244
Julien <jbertazzolambert@gmail.com>
2022-05-05 01:09:37 -0400

    chore: fixed & reformatted inputs

--- client/src/components/Inputs/Time.js
+++ client/src/components/Inputs/Time.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Time(props) {
   return (
     <div>
-        <input type="time" value={props.value} onChange={props.onChange} />
+        <input type="time" value={props.value} onChange={(e) => props.handleChange(e.target.value)} />
     </div>
   )
 }
--- client/src/components/Inputs/TimeRange.js
+++ client/src/components/Inputs/TimeRange.js
@@ -2,11 +2,20 @@ import React from 'react'
 import Time from './Time'
 
 export default function TimeRange(props) {
+
+  const handleChangeFirst = v => {
+    props.handleChange(`${v}_${props.value.split("_")[1]}`)
+  }
+
+  const handleChangeSecond = v => {
+    props.handleChange(`${props.value.split("_")[0]}_${v}`)
+  }
+
   return (
     <div>
-        <Time props={props.value.split("_")[0]} onChange={props.onChangeFirst} />
+        <Time props={props.value.split("_")[0]} handleChange={handleChangeFirst} />
          to 
-        <Time props={props.value.split("_")[1]} onChange={props.onChangeSecond} />
+        <Time props={props.value.split("_")[1]} handleChange={handleChangeSecond} />
     </div>
   )
 }
